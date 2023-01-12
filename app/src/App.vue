2ed7c39bb28774e9d553c81e9249fbe0dada1be1
Modenero Support <support@modenero.com>
2023-01-11 19:11:09 -0500

    Update UI.

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,12 +4,13 @@
 
 <template>
     <main class="w-screen h-screen flex justify-center items-center bg-gradient-to-r from-gray-300 to-gray-500">
-        <section class="flex flex-col items-center">
-            <h1 class="text-6xl text-indigo-900 font-bold">
-                TΞLR Concierge
+        <section class="flex flex-col items-center gap-4">
+            <h1 class="flex flex-col sm:flex-row sm:gap-2 text-indigo-900 font-bold text-center">
+                <span class="text-8xl sm:text-7xl">TΞLR</span>
+                <span class="text-5xl sm:text-7xl text-indigo-700">Concierge</span>
             </h1>
 
-            <h3 class="text-xl text-gray-700 font-medium italic">
+            <h3 class="text-base sm:text-xl text-gray-700 font-medium italic">
                 Under Construction...
             </h3>
         </section>
