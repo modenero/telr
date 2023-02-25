14b679a814d0e5c91f3d846eec03fcc545cc0248
Modenero Support <support@modenero.com>
2023-02-25 00:56:54 -0500

    Refactor from `concierge` to `telr`.

--- concierge-mobile/android/app/src/main/java/app/telr/MainApplication.java
+++ concierge-mobile/android/app/src/main/java/app/telr/MainApplication.java
@@ -1,4 +1,4 @@
-package com.concierge;
+package app.telr;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- concierge-mobile/ios/telrTests/telrTests.m
+++ concierge-mobile/ios/telrTests/telrTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface conciergeTests : XCTestCase
+@interface telrTests : XCTestCase
 
 @end
 
-@implementation conciergeTests
+@implementation telrTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
