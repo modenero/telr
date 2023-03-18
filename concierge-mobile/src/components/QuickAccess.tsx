42f40da17202a97487ec331d7b171ffc21a34c9f
Modenero Support <support@modenero.com>
2023-03-17 23:49:31 -0400

    Update quick button.

--- concierge-mobile/src/components/QuickAccess.tsx
+++ concierge-mobile/src/components/QuickAccess.tsx
@@ -23,7 +23,7 @@ type BlankProps = {
  */
 export default Blank = (props: BlankProps) => {
     return (
-        <View className="mx-3 h-20 flex justify-center items-center bg-rose-500 rounded-lg">
+        <View className="mx-3 h-20 px-3 flex justify-center items-center bg-rose-500 border-4 border-purple-500 rounded-xl">
             <Text className="text-3xl text-purple-100 font-medium">
                 {props.name} - {props.balance}
             </Text>
