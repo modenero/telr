aee832c531a0fceed7228063d7dde5629e4b6faf
Modenero Support <support@modenero.com>
2023-06-29 19:45:55 -0400

    Update auth URL.

--- exchange/components/AuthView.vue
+++ exchange/components/AuthView.vue
@@ -7,7 +7,7 @@ import QRCode from 'qrcode'
 import { useProfileStore } from '@/stores/profile'
 
 /* Set constants. */
-const NEXID_ENDPOINT = 'nexid://nexa.exchange/v1/auth'
+const NEXID_ENDPOINT = 'nexid://app.nexa.exchange/v1/auth'
 
 /* Initialize (reactive) holders. */
 let regLink = ref(null)
