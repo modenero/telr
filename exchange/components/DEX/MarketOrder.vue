25d8596324a3f270918cc15bc35f05fb6338464a
Modenero Support <support@modenero.com>
2023-07-12 08:01:03 -0400

    Refactor.

--- exchange/components/DEX/MarketOrder.vue
+++ exchange/components/DEX/MarketOrder.vue
@@ -1,27 +1,54 @@
 <script setup lang="ts">
 /* Import modules. */
-import { ref } from 'vue'
+import { getTokenInfo } from '@nexajs/rostrum'
 
 const isShowingDeposit = ref(true)
 const isShowingWithdraw = ref(false)
 const isShowingStaking = ref(false)
 
 const assetid = ref(null)
+const assetName = ref(null)
 
 /* Initialize route. */
 const route = useRoute()
 // console.log('ROUTE', route)
-
 console.log('PARAMS', route.params)
+
 /* Set (route) path. */
 assetid.value = route.params.assetid
 console.log('assetid', assetid.value)
+
+
+const init = async () => {
+    let info
+
+    if (assetid.value.length > 6) {
+        info = await getTokenInfo(assetid.value)
+        console.log('TOKEN INFO', info)
+    } else {
+        assetName.value = `Ava's Cash`
+    }
+
+    if (info) {
+        assetName.value = info.name
+    }
+}
+
+onMounted(() => {
+    init()
+})
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+
 </script>
 
 <template>
     <main class="px-2 py-1 bg-gradient-to-b from-gray-900 to-gray-700 border-2 border-gray-500 rounded-lg">
         <div class="text-lg text-yellow-500 font-medium">
-            Instant Buy {{assetid}}
+            Instant Buy {{assetName}}
         </div>
 
         <h2 class="flex text-base gap-1">
--- exchange/components/DEX/Pools/TopFeed.vue
+++ exchange/components/DEX/Pools/TopFeed.vue
@@ -7,19 +7,30 @@ const init = async () => {
 
     pools.value.push({
         name: 'AVAS/NEXA',
-        summary: `Ava's are the official token of Ava's DAO`,
-        price: '$0.33888 per $NEXA',
+        summary: `Daily payouts to HODLers`,
+        price: '47,620 NEXA per AVAS',
         slug: 'AVAS',
         tokenid: 'nexa:tptlgmqhvmwqppajq7kduxenwt5ljzcccln8ysn9wdzde540vcqqqcra40x0x',
         tokenidHex: '57f46c1766dc0087b207acde1b3372e9f90b18c7e67242657344dcd2af660000',
+        ticker: 'AVAS',
     })
 
     pools.value.push({
         name: 'MUD/NEXA',
-        summary: `MudGroup are cool!`,
-        price: '$0.00 per $NEXA',
+        summary: `MudGroupies are cool!`,
+        price: '0 NEXA per MUD',
         tokenid: 'nexa:trs2ccuvqmf62853mwt7ly478qkhjugrk92kfl6cf75yk8hssyqqqlcpr87w8',
         tokenidHex: 'e0ac638c06d3a51e91db97ef92be382d797103b15564ff584fa84b1ef0810000',
+        ticker: 'MUD',
+    })
+
+    pools.value.push({
+        name: 'POWF/NEXA',
+        summary: `PoWfReAk#0 are trending...`,
+        price: '0 NEXA per POWF',
+        tokenid: 'nexa:tqr5nuefyyvtexkfg37cmazk0g8vpym9vexx0t8s4yjjqm0gm5qqqd07j4h7k',
+        tokenidHex: '0749f3292118bc9ac9447d8df4567a0ec09365664c67acf0a925206de8dd0000',
+        ticker: 'POWF',
     })
 }
 
