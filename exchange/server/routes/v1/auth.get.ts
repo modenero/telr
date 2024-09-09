6efb209116ed69ce5cc392cfe3f50b5ce10376c4
Modenero Support <support@modenero.com>
2024-09-09 01:23:52 -0400

    Add context.

--- exchange/server/routes/v1/auth.get.ts
+++ exchange/server/routes/v1/auth.get.ts
@@ -1,9 +1,10 @@
 /* Import modules .*/
 import { v4 as uuidv4 } from 'uuid'
 
-export default defineEventHandler(async (event) => {
+export default defineEventHandler((event) => {
     let params
 
+console.log('CONTEXT', event.context)
     /* Set parameters. */
     params = event.context.params
     console.log('AUTH PARAMS', params)
