823b00b85b279dacd2c9aec94a5dfe5dd02f6682
Modenero Support <support@modenero.com>
2024-09-09 01:26:22 -0400

    Test body.

--- exchange/server/routes/v1/auth.get.ts
+++ exchange/server/routes/v1/auth.get.ts
@@ -1,10 +1,17 @@
 /* Import modules .*/
 import { v4 as uuidv4 } from 'uuid'
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
     let params
 
 console.log('CONTEXT', event.context)
+/* Initailize locals. */
+let body
+
+/* Set (request) body. */
+body = await readBody(event)
+console.log('AUTH BODY', body)
+
     /* Set parameters. */
     params = event.context.params
     console.log('AUTH PARAMS', params)
